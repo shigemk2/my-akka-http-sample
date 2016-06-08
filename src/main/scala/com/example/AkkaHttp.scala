a3b71cdf9cc6754948fe7d79dbf9daf751a1e7a6
shigemk2 <i.am.shige@gmail.com>
2016-06-09 00:00:51 +0900

    [WIP] query

--- src/main/scala/com/example/AkkaHttp.scala
+++ src/main/scala/com/example/AkkaHttp.scala
@@ -3,12 +3,9 @@ package com.example
 import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpMethods._
-import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
-
-import scala.io.StdIn
 
 object AkkaHttp extends App {
   implicit val system = ActorSystem()
@@ -18,28 +15,12 @@ object AkkaHttp extends App {
   val config = ConfigFactory.load()
   val logger = Logging(system, getClass)
 
+  import Directives._
 
-  val requestHandler: HttpRequest => HttpResponse = {
-    case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
-      HttpResponse(entity = HttpEntity(
-        ContentTypes.`text/html(UTF-8)`,
-        "<html><body>Hello world!</body></html>"))
+  val route =
+    parameters('color, 'backgroundColor) { (color, backgroundColor) =>
+      complete(s"The color is '$color' and the background is '$backgroundColor'")
+    }
 
-    case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
-      HttpResponse(entity = "PONG!")
-
-    case HttpRequest(GET, Uri.Path("/crash"), _, _, _) =>
-      sys.error("BOOM!")
-
-    case _: HttpRequest =>
-      HttpResponse(404, entity = "Unknown resource!")
-  }
-
-  val bindingFuture = Http().bindAndHandleSync(requestHandler, config.getString("http.interface"), config.getInt("http.port"))
-  println(s"Server online at http://${config.getString("http.interface")}:${config.getInt("http.port")}/\nPress RETURN to stop...")
-  StdIn.readLine() // let it run until user presses return
-  bindingFuture
-    .flatMap(_.unbind()) // trigger unbinding from the port
-    .onComplete(_ => system.terminate()) // and shutdown when done
-
+  val bindingFuture = Http().bindAndHandle(route, config.getString("http.interface"), config.getInt("http.port"))
 }
